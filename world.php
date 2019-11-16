c199b4a6748ebe48e0762af2281891222de621d6
Tassan Stewart <tassanstewart17@gmail.com>
2019-11-16 21:49:03 +0000

    Second Commit

--- world.php
+++ world.php
@@ -13,12 +13,65 @@ $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
 
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
+echo "<style>
+table{
+	font-family: Geneva, sans-serif;
+	font-size: 1.1em;	
+}
+
+tr:nth-child(even){
+	background-color: lightblue;
+}
+
+
+tr:nth-child(odd){
+	background-color: white;
+}
+
+th{
+	background-color: white
+	color: white;
+	text-align: left;
+	padding: 0 10px 0 5px;
+}
+
+td{
+	text-align: left;
+	padding: 5px;5px;5px;5px;
+}
+ </style>";
+
+echo "<table>";	
+echo '<tr>';
+
+echo '<th> Country Name</th>
+
+      <th> Continent</th>
+
+      <th> Independence Year</th>
+
+      <th> Head of State</th>';
+
+echo '</tr>';
+foreach ($results as $row) {
+
+  echo '<tr>';
+
+  echo '<td>' . $row['name']. '</td>';
+
+  echo '<td>' . $row['continent']. '</td>';
+
+  echo '<td>' . $row['independence_year']. '</td>';
+
+  echo '<td>' . $row['head_of_state']. '</td>';
+
+  echo '</tr>';
+}
+
+  echo '</table>';
 
 ?>
-<ul>
-<?php foreach ($results as $row): ?>
-  <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
-<?php endforeach; ?>
-</ul>
+
+
 
 

5c28e2ccd3b85a6ede5bf864900e036067adf149
tassanstewart <tassanstewart17@gmail.com>
2019-11-18 17:50:39 +0000

    Third Commit

--- world.js
+++ world.js
@@ -2,32 +2,43 @@ window.onload = myFunction;
 
 function myFunction() {
 	var element = document.getElementById("lookup");
+	var element2 = document.getElementById("lookup2");
 	var divElement = document.getElementById("result");
-    
-    element.onclick = function(){
-    alert('...jghjgjgjgjhg..');
-    
-    var httpRequest = new XMLHttpRequest();
-	var url = "http://localhost:8080/world.php";
-	httpRequest.open('GET', url, true)
+	var url;
+	var country;
 	
-	httpRequest.onreadystate = function(){
-		
-		if (httpRequest.readyState === XMLHttpRequest.DONE){
-			if (httpRequest.status === 200) {
+    element.onclick = function(){
+    	country = document.getElementById("country").value;
+    	url = "https://e2eb694e20814d80ac20af2aa2e13058.vfs.cloud9.us-east-1.amazonaws.com/world.php?country="+country;
+    	clickFunction(url);
+    };
+    
+    element2.onclick = function(){
+    	country = document.getElementById("country").value;
+    	url = "https://e2eb694e20814d80ac20af2aa2e13058.vfs.cloud9.us-east-1.amazonaws.com/world.php?country="+country+"&context=cities";
+    	clickFunction(url);
+    }
+    
+    function clickFunction(url){
+	
+    
+    var request = new XMLHttpRequest();
+	
+	
+	request.open("GET", url, true);
+	request.send();	
+	
+    request.onreadystatechange = function(){
+		if(this.readyState === 4 && this.status === 200) {
 			    
-			var response = httpRequest.responseText;
+			var response = request.responseText;
 			divElement.innerHTML = response;
 		}
 		else{
 			divElement.innerHTML = "Error";
 		}
+	};
 	
-	
-	httpRequest.open("POST",url,true);
-	httpRequest.send();}
-		
-	}
-		
-    //}
 }
+}
+
--- world.php
+++ world.php
@@ -7,11 +7,76 @@ $dbname = 'world';
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
 
 $country = $_GET['country'];
+$context = $_GET['context'];
 
-$stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
+if(isset($context) && ($context==="cities")){
+    $stmt = $conn->query("SELECT c.name as city, c.district as district, c.country_code, cs.name as
+country, c.population as population FROM cities c join countries cs on
+c.country_code = cs.code WHERE cs.name = '$country'");
+
+$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+echo "<table>";	
+echo '<tr>';
+
+echo '<th> Name</th>
+
+      <th> District</th>
+
+      <th> Population</th>';
+
+echo '</tr>';
+foreach ($result as $row) {
+
+  echo '<tr>';
+
+  echo '<td>' . $row['city']. '</td>';
+
+  echo '<td>' . $row['district']. '</td>';
+
+  echo '<td>' . $row['population']. '</td>';
+
+  echo '</tr>';
+}
+  echo '</table>';
+  
+}
 
 
-$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+else{
+    $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
+    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    
+    
+echo "<table>";	
+echo '<tr>';
+
+echo '<th> Country Name</th>
+
+      <th> Continent</th>
+
+      <th> Independence Year</th>
+
+      <th> Head of State</th>';
+
+echo '</tr>';
+foreach ($results as $row) {
+
+  echo '<tr>';
+
+  echo '<td>' . $row['name']. '</td>';
+
+  echo '<td>' . $row['continent']. '</td>';
+
+  echo '<td>' . $row['independence_year']. '</td>';
+
+  echo '<td>' . $row['head_of_state']. '</td>';
+
+  echo '</tr>';
+}
+
+  echo '</table>';
+}
 
 echo "<style>
 table{
@@ -41,34 +106,7 @@ td{
 }
  </style>";
 
-echo "<table>";	
-echo '<tr>';
 
-echo '<th> Country Name</th>
-
-      <th> Continent</th>
-
-      <th> Independence Year</th>
-
-      <th> Head of State</th>';
-
-echo '</tr>';
-foreach ($results as $row) {
-
-  echo '<tr>';
-
-  echo '<td>' . $row['name']. '</td>';
-
-  echo '<td>' . $row['continent']. '</td>';
-
-  echo '<td>' . $row['independence_year']. '</td>';
-
-  echo '<td>' . $row['head_of_state']. '</td>';
-
-  echo '</tr>';
-}
-
-  echo '</table>';
 
 ?>
 
